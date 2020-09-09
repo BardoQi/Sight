9242ac6ceef7d4ecabc00579863b0dc65edc113e
Bardo <bardoqi@gmail.com>
2020-09-09 09:34:13 +0800

    3RD COMMIT

---
+++ src/Enums/RelationEnum.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+/*
+ * This file is part of the bardoqi/sight package.
+ *
+ * (c) BardoQi <67158925@qq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Bardoqi\Sight\Enums;
+
+/**
+ * Class RelationEnum
+ *
+ * @package Bardoqi\Sight\Enums
+ */
+final class RelationEnum
+{
+    const HAS_ONE = 1;
+    const HAS_MANY =2;
+}
