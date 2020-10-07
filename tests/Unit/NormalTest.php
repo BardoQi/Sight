b5cca70ef92f7004c70ec4e522743e80c2a4643b
Bardo <bardoqi@gmail.com>
2020-10-07 12:07:01 +0800

    test add function

--- tests/Unit/NormalTest.php
+++ tests/Unit/NormalTest.php
@@ -135,4 +135,32 @@ final class NormalTest extends TestCase
         $data = $NormalPresenter->toPaginateArray(PaginateTypeEnum::PAGINATE_API);
         $this->assertTrue($data['current_page'] === 1);
     }
+
+    /** @test */
+    public function addFunctionTest()
+    {
+        $NormalPresenter = NormalPresenter::of();
+        $NormalPresenter->addFunction("area_of",function ($value){
+            return number_format($value,2) . " ㎡";
+        });
+        $this->assertTrue($NormalPresenter->hasMethod('area_of'));
+        // test call
+        $test_area = $NormalPresenter->area_of(3.1415);
+        $this->assertTrue("3.14 ㎡" === $test_area);
+
+        $NormalPresenter->addMethod("area_value",function ($value){
+            return number_format($value,2) . " ㎡";
+        });
+        $this->assertTrue($NormalPresenter->hasMethod('area_value'));
+        // test call
+        $test_area = $NormalPresenter->area_value(3.1415);
+        $this->assertTrue("3.14 ㎡" === $test_area);
+
+        $NormalPresenter->addFormatter("toArea",function ($value){
+            return number_format($value,2) . " ㎡";
+        });
+        $formatter = $NormalPresenter->data_formatter;
+        $test_area = call_user_func_array([$formatter, 'format'], ["toArea", 3.1415]);
+        $this->assertTrue("3.14 ㎡" === $test_area);
+    }
 }
